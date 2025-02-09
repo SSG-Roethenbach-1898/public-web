bcbe9550275e48e391cb79062f0a3373aa5d4cf1
mox3000 <ensexhaust@gmail.com>
2025-02-09 16:21:17 +0100

    Ausversehen kaputt gemacht

--- frontend/assets/scripts/components.js
+++ frontend/assets/scripts/components.js
@@ -244,7 +244,8 @@ export class ContactForm extends LitElement {
     }
 }
 
-customElements.define('contact-form', ContactForm);
+// Ups Kaputt!
+c//ustomElements.define('contact-form', ContactForm);
 
 class FooterContent extends LitElement {
     static styles = css`
