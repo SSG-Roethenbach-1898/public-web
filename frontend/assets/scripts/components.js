b616b820b0a0709d05d82294b979dff634da9c4c
Thraax Session <thraax.session@iatlas.technology>
2025-03-09 18:57:23 +0100

    Remove unused image template code from GalleryCarousel component

--- frontend/assets/scripts/components.js
+++ frontend/assets/scripts/components.js
@@ -360,15 +360,6 @@ class GalleryCarousel extends LitElement {
     }
 
     render() {
-        const imageTemplate = [];
-        for (const image of this.#galleryController.images) {
-            imageTemplate.push(html`
-                <div class="carousel-item">
-                    <img class="img-fluid" src="/static/img/gallery/${image}" alt="${image}">
-                </div>
-            `);
-        }
-
         return html`
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
                 rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
