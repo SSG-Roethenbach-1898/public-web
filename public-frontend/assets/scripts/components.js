23a6c960a27251aad7acbedd2ff456ace1bb56e7
Thraax Session <thraax.session@iatlas.technology>
2025-08-02 22:21:37 +0200

    Update footer copyright text and add informational message in contact page and improved background color for dark mode

--- public-frontend/assets/scripts/components.js
+++ public-frontend/assets/scripts/components.js
@@ -339,7 +339,7 @@ class FooterContent extends LitElement {
                 </div>
 
                 <div class="text-center p-1">
-                    <p>&copy; ${new Date().getFullYear()} SSG Röthenbach 1898</p>
+                    <p>&copy;1898 - ${new Date().getFullYear()} SSG Röthenbach 1898 e.V.</p>
                 </div>
             </footer>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
